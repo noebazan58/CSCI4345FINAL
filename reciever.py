1cc2c3c4eef0d706ba8f1c042dc52ffdd36680c4
noebazan58 <147530837+noebazan58@users.noreply.github.com>
2026-05-14 21:33:02 -0500

    Update reciever.py

--- reciever.py
+++ reciever.py
@@ -6,11 +6,11 @@ def handle_frame(frame):
     if frame.haslayer(Raw):
         message = frame[Raw].load.decode(errors="ignore")
 
-        print("\n--- New Ethernet Chat Message ---")
+        print("\n--- New Message")
         print(f"From MAC: {frame.src}")
         print(f"To MAC:   {frame.dst}")
         print(f"Message:  {message}")
-        print("---------------------------------")
+        print("")
 
 print("Ethernet Chat Receiver Started")
 print(f"Listening on interface: {INTERFACE}")
